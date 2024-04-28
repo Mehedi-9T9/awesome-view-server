13a9fc47cf13ac6f793c44a0506541aa0b016c75
Mehedi-9T9 <mehedi.hasan9t9@gmail.com>
2024-04-28 20:10:43 +0600

    update data sent to client successfull

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express()
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 require('dotenv').config()
 
 // midleware
@@ -9,8 +9,8 @@ require('dotenv').config()
 
 
 
-// uHXQwYTwuL7n3GZi
-app.use(cors)
+
+app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
@@ -19,7 +19,7 @@ app.get('/', (req, res) => {
 // console.log(process.env.DB_USER);
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cd4uzfy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -35,6 +35,38 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+
+        const database = client.db("awesome_viewDB");
+        const awesome_viewCollaction = database.collection("awesome_view");
+        //sent data client
+        app.get('/allTourismSpot', async (req, res) => {
+            const cursor = awesome_viewCollaction.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        app.get('/updateTourismSpot/:id', async (req, res) => {
+            console.log(req.params.id);
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) };
+            const result = await awesome_viewCollaction.findOne(query)
+            res.send(result)
+        })
+
+
+        //sent database
+        app.post('/addTourismSpot', async (req, res) => {
+            const data = req.body
+            // console.log(data);
+            const result = await awesome_viewCollaction.insertOne(data);
+            res.send(result)
+        })
+
+
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
